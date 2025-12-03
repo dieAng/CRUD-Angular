bb796d516701a9e1256ff4a4abb1d052e9c41672
DAM204 <dam01.2025@gmail.com>
2025-12-03 11:20:42 +0100

    asd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './post/index/index';
+import { ViewComponent } from './post/view/view';
+import { CreateComponent } from './post/create/create';
+import { EditComponent } from './post/edit/edit';
+
+export const routes: Routes = [
+  { path: 'post', redirectTo: 'post/index', pathMatch: 'full' },
+  { path: 'post/index', component: IndexComponent },
+  { path: 'post/:postId/view', component: ViewComponent },
+  { path: 'post/create', component: CreateComponent },
+  { path: 'post/:postId/edit', component: EditComponent },
+];
---
+++ src/app/post/view/view.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+import { PostService } from '../post-s';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './view.html',
+  styleUrl: './view.css',
+})
+export class ViewComponent {
+  id!: number;
+  post!: Post;
+
+  /*------------------------------------------
+  --------------------------------------------
+  Created constructor
+  --------------------------------------------
+  --------------------------------------------*/
+  constructor(
+    public postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+    });
+  }
+}
