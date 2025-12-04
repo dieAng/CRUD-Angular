9702565e706a6c696055ee70f401f1539a46fb9a
DAM204 <dam01.2025@gmail.com>
2025-12-04 08:44:11 +0100

    finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CreateComponent } from './post/create/create';
 import { EditComponent } from './post/edit/edit';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'post/index', pathMatch: 'full' },
   { path: 'post', redirectTo: 'post/index', pathMatch: 'full' },
   { path: 'post/index', component: IndexComponent },
   { path: 'post/:postId/view', component: ViewComponent },
